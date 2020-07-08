28df634f75e118dd1b371112c3e619eb8c8fa411
Dariusz Baltaziuk <dariusz@baltaziuk.eu>
2020-07-08 15:38:34 +0200

    6.3 this

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@
   });*/
   
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /*[DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -15,9 +16,12 @@
         activeLink.classList.remove('active');
     }
   
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
+
+    /*[DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('posts span.active');
 
